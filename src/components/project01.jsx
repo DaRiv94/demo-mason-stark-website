880f0afd377becf9714f7fe5ad15bb220db74899
Frankie Riviera <frankgriviera@outlook.com>
2025-07-17 09:33:07 -0400

    added Logic App form responder

--- src/components/project01.jsx
+++ src/components/project01.jsx
@@ -7,15 +7,21 @@ export const Project01 = (props) => {
         <div className="row">
           <div className="col-xs-12 col-md-6">
             {" "}
-            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
+            <img src="img/portfolio/LogicAppFormResponder.png" className="img-responsive" alt="" />{" "}
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
-              <h2>Portfolio Project 01</h2>
+              <h2>Logic App Form Responder</h2>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
+                              <a
+                  href="https://forms.office.com/Pages/ResponsePage.aspx?id=n4MNJxFb8U-Jmqt9KJQ4a_gUhpDVCRVJikfVBGm0odhUQjlZV0kxTU5UVUw4Vk02MjRYWVNFSEFBRC4u"
+                  className="btn btn-custom btn-lg "
+                >
+                  Try It Out
+                </a>
               <h3>Features</h3>
               <div className="list-style">
-                <div className="col-lg-6 col-sm-6 col-xs-12">
+                <div className="col-lg-12 col-sm-12 col-xs-12">
                   <ul>
                     {props.data
                       ? props.data.Features1.map((d, i) => (
@@ -24,7 +30,7 @@ export const Project01 = (props) => {
                       : "loading"}
                   </ul>
                 </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
+                {/* <div className="col-lg-6 col-sm-6 col-xs-12">
                   <ul>
                     {props.data
                       ? props.data.Features2.map((d, i) => (
@@ -32,7 +38,7 @@ export const Project01 = (props) => {
                         ))
                       : "loading"}
                   </ul>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
